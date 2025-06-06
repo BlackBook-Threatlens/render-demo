4649c78858c58fa1be7381d082302f600ec53cbd
BlackBook-Threatlens <shubhanumuthukumar.sit.comp@gmail.com>
2025-06-06 21:27:38 +0530

    Update xss_flask.py

--- xss_flask.py
+++ xss_flask.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from xss_model import predict  # Import the predict function
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict_route():
