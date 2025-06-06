bb3d53d070be7cfc26a2894fd2a05d6b8b5aa2e5
BlackBook-Threatlens <shubhanumuthukumar.sit.comp@gmail.com>
2025-06-06 23:01:15 +0530

    Update xss_flask.py

--- xss_flask.py
+++ xss_flask.py
@@ -23,4 +23,4 @@ def predict_route():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # debug=True for development
+    app.run(host='0.0.0.0', port=10000, debug=True)  # debug=True for development
